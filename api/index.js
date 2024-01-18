9e9c6ab303dbe67fd57919bba4ae26e9a23cd09a
ghada1234 <ghadaabdulaziz1@gmail.com>
2024-01-18 18:55:59 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
